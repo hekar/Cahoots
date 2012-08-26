c6707732edf8f9fba93cf62b2adf21de625656c4
hekar <hekar@hekar-P5K-SE.(none)>
2012-08-26 16:30:01 -0400

    Added jOOQ scripts and first sql evolution
    jOOQ scripts have been added to to the cahoots-server
    Evolutions are for postgres as that is the main development database

--- cahoots-server/project/Build.scala
+++ cahoots-server/project/Build.scala
@@ -1,40 +1,37 @@
 import sbt._
+import sbt.IO._
 import Keys._
 import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "cahootServer"
-    val appVersion      = "1.0-SNAPSHOT"
-     
-    val appDependencies = Seq(
-      "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
-      "com.typesafe" % "slick_2.10.0-M6" % "0.11.0",
-      "org.jooq" % "jooq" % "2.4.0"
-    )
-    
-    /**
-     * Generate the jOOQ database classes
-     */
-    val jooq = TaskKey[Unit]("jooq", "Generate the jOOQ class files")
-    val jooqTask = jooq := {
-      println("Testing. Jooq")
-      "java -classpath jooq-2.4.0.jar;jooq-meta-2.4.0.jar;jooq-codegen-2.4.0.jar;mysql-connector-java-5.1.18-bin.jar;. org.jooq.util.GenerationTool /guestbook.xml" !
-    }
+  val appName = "cahootServer"
+  val appVersion = "1.0-SNAPSHOT"
 
-       retrieveManaged := true
-    /**
-     * Add your own project settings here
-     */
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-       // ==============================================
-       // Tasks
-       // ==============================================
-       jooqTask
- 
-       // ==============================================
-       // Settings
-       // ==============================================
-    )
+  val appDependencies = Seq(
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
+    "com.typesafe" % "slick_2.10.0-M6" % "0.11.0",
+    "org.jooq" % "jooq" % "2.4.0"
+  )
+
+  /**
+   * Generate the jOOQ database classes
+   */
+  val jooq = TaskKey[Unit]("jooq", "Generate the jOOQ class files")
+  val jooqTask = jooq := {
+    println("Downloading jOOQ jars")
+    sys.process.Process(Seq("jooq/download_jooq.sh")) !!
+    
+    println("Generating jOOQ classes")
+    sys.process.Process(Seq("jooq/generate_jooq.sh")) !!
+  }
+
+  /**
+   * Add your own project settings here
+   */
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    jooqTask
+
+  )
 
 }
