90b3ac82c61791135e96ab95ba105cc06f366ba5
Samuel Lewis <samuelewis@gmail.com>
2012-10-29 12:20:25 -0400

    Crappy logged in user list

--- projects/cahoots-server/app/controllers/Application.scala
+++ projects/cahoots-server/app/controllers/Application.scala
@@ -3,6 +3,8 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.cache._
+import scala.collection.mutable._
+import models._
 
 object Application extends Controller {
   
@@ -11,7 +13,19 @@ object Application extends Controller {
     val pass = request.body.asFormUrlEncoded.get.get("password").get.apply(0)
     
     // TODO: do actual authenticaty stuff here...
+    var users:ListBuffer[ActiveUser] = Cache.get("users").asInstanceOf[ListBuffer[ActiveUser]]
     
+    if(users == null)
+    {
+      users = new ListBuffer[ActiveUser]
+    }
+    
+    var stored_user = users.findIndexOf(x => x.username == user)
+    
+    if(stored_user != -1)
+    {
+      users.remove(stored_user)
+    }
     
     var token:String = Cache.get(user + pass).asInstanceOf[String]
     
@@ -23,19 +37,36 @@ object Application extends Controller {
       
     if (token != null)
     {
-      
+      users.append(new ActiveUser(user, token))
+      Cache.set("users", users)
       Ok(token)
     }
     else
     {
       Unauthorized("Invalid username/password")
     }
+    
   }
   
   def deauthenticate = Action { request =>
-    val token = request.body.asFormUrlEncoded.get.get("token").get.apply(0)
+    val token = request.body.asFormUrlEncoded.get.get("auth_token").get.apply(0)
     
     // TODO: do actual deauthentication
+     var users:ListBuffer[ActiveUser] = Cache.get("users").asInstanceOf[ListBuffer[ActiveUser]]
+    
+    if(users == null)
+    {
+      users = new ListBuffer[ActiveUser]
+    }
+    
+    var stored_user = users.findIndexOf(x => x.token == token)
+    
+    if(stored_user != -1)
+    {
+      users.remove(stored_user)
+    }
+    
+    
     Ok("")
     //Unauthorized("Unrecognized request token")
   }
