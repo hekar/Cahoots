9fc2d6e5313e9da1b045c4e1b8bdeb44769809e3
Samuel Lewis <samuelewis@gmail.com>
2012-11-20 14:12:11 -0500

    Updated python script to connect with a websocket and added a simple listening script

--- projects/scripts/in_out.py
+++ projects/scripts/in_out.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
+#
+# Requires:
+#   https://github.com/liris/websocket-client - install from source released version if buggy
+#   http://docs.python-requests.org/en/latest/
+#
 
 import requests
 import os
 import time
+from websocket import create_connection
 
 if __name__ == "__main__":
     while True:
@@ -12,11 +18,17 @@ if __name__ == "__main__":
                 'password': str(os.getpid())
             }).text
         print "Logged in as {0}:{1}".format(os.getpid(), authToken)
+        
+        ws = create_connection('ws://localhost:9000/app/message?auth_token=' + authToken)
         time.sleep(5)
+        
         requests.post(
         "http://localhost:9000/app/logout", data={
             'auth_token': authToken
         }).text
+        
         print "Logged out"
+        ws.close()
         time.sleep(5)
+        
 
---
+++ projects/scripts/listen.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#
+# Requires:
+#   https://github.com/liris/websocket-client - install from source released version if buggy
+#   http://docs.python-requests.org/en/latest/
+#
+
+
+import websocket
+import requests
+import thread
+import time
+import sys
+import os
+
+def on_message(ws, message):
+    print message
+
+def on_error(ws, error):
+    print error
+
+def on_close(ws):
+    print "### closed ###"
+
+def on_open(ws):
+    print '### opened ###'
+
+if __name__ == "__main__":
+    authToken = requests.post(
+        "http://localhost:9000/app/login", data={
+            'username': str(os.getpid()),
+            'password': str(os.getpid())
+        }).text
+    print "Logged in as {0}:{1}".format(os.getpid(), authToken)
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp('ws://localhost:9000/app/message?auth_token=' + authToken,
+            on_message = on_message,
+            on_error = on_error,
+            on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
