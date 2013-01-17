71224e33de2d0d5cc256fca77e5a6a2d7448cdee
tkempton <kempton.tom@gmail.com>
2013-01-16 21:21:43 -0500

    server side chat done

---
+++ projects/cahoots-vs/src/CahootsService/MessageModels/Chat/SendChatMessage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Cahoots.Services.MessageModels
+{
+    [DataContract]
+    public class SendChatMessage : MessageBase
+    {
+        /// <summary>
+        /// Gets or sets to.
+        /// </summary>
+        /// <value>
+        /// To.
+        /// </value>
+        [DataMember(Name = "to", IsRequired = true)]
+        public string To { get; set; }
+
+        /// <summary>
+        /// Gets or sets from.
+        /// </summary>
+        /// <value>
+        /// From.
+        /// </value>
+        [DataMember(Name = "from", IsRequired = true)]
+        public string From { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        /// <value>
+        /// The message.
+        /// </value>
+        [DataMember(Name = "message", IsRequired = true)]
+        public string Message { get; set; }
+    }
+}
