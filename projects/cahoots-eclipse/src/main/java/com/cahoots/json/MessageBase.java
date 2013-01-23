f5722ee3fe4fb87bb951e1982b656f54db94f2fd
Hekar <hekark@gmail.com>
2013-01-23 18:36:39 -0500

    refactored ton of code and added logging

--- projects/cahoots-eclipse/src/main/java/com/cahoots/json/MessageBase.java
+++ projects/cahoots-eclipse/src/main/java/com/cahoots/json/MessageBase.java
@@ -1,9 +1,21 @@
 package com.cahoots.json;
 
 public class MessageBase {
-	
-	public MessageBase(){}
-	
-	public String service;
-	public String type;
+
+	private final String service;
+	private final String type;
+
+	protected MessageBase(String service, String type) {
+		this.service = service;
+		this.type = type;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public String getType() {
+		return type;
+	}
+
 }
