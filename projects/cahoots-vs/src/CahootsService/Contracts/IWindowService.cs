b75862cf315f9ca2596f006f769cfbf1fceec457
tkempton <kempton.tom@gmail.com>
2013-01-27 19:18:44 -0500

    vs: chat windows open when you receive a message

---
+++ projects/cahoots-vs/src/CahootsService/Contracts/IWindowService.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Cahoots.Services.Contracts
+{
+    using Cahoots.Services.Models;
+
+    public interface IWindowService
+    {
+        /// <summary>
+        /// Opens a chat window.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        void OpenChatWindow(Collaborator user);
+
+        /// <summary>
+        /// Opens a chat window.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        void OpenChatWindow(string username);
+    }
+}
