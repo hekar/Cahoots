1501e53fca3801235f39c9ffedd3eeaa275ff9d1
Samuel Lewis <samuelewis@gmail.com>
2013-02-07 20:51:59 -0500

    Database update script for chat stuff

---
+++ projects/cahoots-sql/versions/4/chat.sql
@@ -0,0 +1,44 @@
+-- Table: chat
+
+-- DROP TABLE chat;
+
+CREATE TABLE chat
+(
+  "from" character varying(255) NOT NULL,
+  "to" character varying(255) NOT NULL,
+  message text NOT NULL,
+  date time with time zone,
+  id serial NOT NULL,
+  CONSTRAINT chat_pkey PRIMARY KEY (id),
+  CONSTRAINT from_fk FOREIGN KEY ("from")
+      REFERENCES users (username) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT to_fk FOREIGN KEY ("to")
+      REFERENCES users (username) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT u_id UNIQUE (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE chat
+  OWNER TO postgres;
+
+-- Index: fki_from_fk
+
+-- DROP INDEX fki_from_fk;
+
+CREATE INDEX fki_from_fk
+  ON chat
+  USING btree
+  ("from" COLLATE pg_catalog."default");
+
+-- Index: fki_to_fk
+
+-- DROP INDEX fki_to_fk;
+
+CREATE INDEX fki_to_fk
+  ON chat
+  USING btree
+  ("to" COLLATE pg_catalog."default");
+
