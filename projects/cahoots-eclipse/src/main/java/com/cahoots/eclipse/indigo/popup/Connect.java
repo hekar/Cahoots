a8ec3524283176dd2a7d4d0ae4ce0af741de024c
Samuel Lewis <samuelewis@gmail.com>
2013-02-12 19:49:42 -0500

    Not disconnection properly

--- projects/cahoots-eclipse/src/main/java/com/cahoots/eclipse/indigo/popup/Connect.java
+++ projects/cahoots-eclipse/src/main/java/com/cahoots/eclipse/indigo/popup/Connect.java
@@ -1,5 +1,11 @@
 package com.cahoots.eclipse.indigo.popup;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
@@ -7,6 +13,7 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 import com.cahoots.connection.CahootsConnection;
+import com.cahoots.connection.ConnectionDetails;
 import com.cahoots.connection.websocket.CahootsSocket;
 import com.cahoots.eclipse.Activator;
 import com.cahoots.eclipse.collab.ConnectDialog;
@@ -57,7 +64,36 @@ public class Connect implements IWorkbenchWindowActionDelegate {
 							"Would you like to disconnect before connecting to another server?");
 
 			if (prompt == MessageDialogStatus.OK) {
-				socket.disconnect();
+
+				HttpClient client = new HttpClient();
+				PostMethod method = new PostMethod("http://"
+						+ connection.getServer() + "/app/logout");
+				List<NameValuePair> data = new LinkedList<NameValuePair>();
+				data.add(new NameValuePair("auth_token", connection
+						.getAuthToken()));
+
+				method.setRequestBody(data.toArray(new NameValuePair[data
+						.size()]));
+				try {
+					int statusCode = client.executeMethod(method);
+					if (statusCode == 200) {
+						connection
+								.updateConnectionDetails(new ConnectionDetails(
+										"", "", "", ""));
+						socket.disconnect();
+					} else {
+						org.eclipse.jface.dialogs.MessageDialog
+								.openInformation(null, "Disconnect Error",
+										"Error disconnecting from server");
+					}
+
+				} catch (Exception ex) {
+					org.eclipse.jface.dialogs.MessageDialog.openInformation(
+							null,
+							"Disconnect Error",
+							"Error disconnecting from server. "
+									+ ex.getMessage());
+				}
 			}
 		}
 
